2c50f7706ea67a72e47f86d7720c78797a95198d
Jan Peterka <jan.peterka@hey.com>
2021-04-20 22:54:27 +0200

    Simplify user

--- app/models/users.py
+++ app/models/users.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from flask_security.models.fsqla_v2 import FsUserMixin as UserMixin
-from flask_security.utils import hash_password
 
 from app import db
 
@@ -12,10 +11,6 @@ class User(db.Model, BaseMixin, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
     created_at = db.Column(db.DateTime, default=datetime.now)
 
     roles = db.relationship(
@@ -23,18 +18,3 @@ class User(db.Model, BaseMixin, UserMixin):
         secondary="users_have_roles",
         backref=db.backref("users", lazy="dynamic"),
     )
-
-
-def create(self):
-    from app import user_datastore
-
-    user_datastore.create_user(
-        email=self.email,
-        password=hash_password(self.password),
-        name=self.name,
-        nickname=self.nickname,
-        choir_id=self.choir_id,
-    )
-    db.session.commit()
-
-    # WIP return user object

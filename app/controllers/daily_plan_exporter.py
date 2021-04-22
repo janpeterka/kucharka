66cd7e06e3767bc83c8357951f0fac39e99b03b2
Jan Peterka <jan.peterka@hey.com>
2021-04-22 13:23:10 +0200

    Add tox.ini

--- app/controllers/daily_plan_exporter.py
+++ app/controllers/daily_plan_exporter.py
@@ -1,18 +1,6 @@
-# import datetime
-
 from flask import redirect, url_for, request
-from flask import render_template as template
-
-# from flask_classful import route
-# from flask_login import current_user, login_required
-
-# from app.helpers.formaters import parse_date
-
-# from app.data.texts import texts
 
 from app.models.daily_plans import DailyPlan
-
-# from app.models.recipes import Recipe
 
 from app.helpers.extended_flask_view import ExtendedFlaskView
 
@@ -32,7 +20,6 @@ class DailyPlanExporterView(ExtendedFlaskView):
 
     def show(self, date_from, date_to):
         self.daily_plans = DailyPlan.load_by_date_range(date_from, date_to)
-        # print(self.daily_plans)
         self.ingredients = DailyPlan.load_ingredient_amounts_for_daily_plans(
             [dp.id for dp in self.daily_plans]
         )

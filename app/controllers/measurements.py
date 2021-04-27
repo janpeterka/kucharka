3a870af9c2f5706ea774840f0a1a29d056862432
Jan Peterka <jan.peterka@hey.com>
2021-04-27 15:31:23 +0200

    Improve turbo in measurements

--- app/controllers/measurements.py
+++ app/controllers/measurements.py
@@ -15,43 +15,80 @@ from app.helpers.extended_flask_view import ExtendedFlaskView
 class MeasurementsView(ExtendedFlaskView):
     decorators = [login_required, admin_required]
 
-    @route("/", methods=["GET", "POST"])
-    def index(self):
+    def before_request(self, name, id=None, *args, **kwargs):
+        super().before_request(name, id, *args, **kwargs)
         self.measurements = Measurement.load_all()
+
+    @route("/show_edit/<id>", methods=["POST"])
+    def show_edit(self, id):
+        # Use this while edit:GET doesn't support stream (probably until WebSocket support)
+        measurement = Measurement.load(id)
         if request.method == "POST":
-            measurement = Measurement(name=request.form["measurement"])
-            measurement.save()
-            self.measurements.append(measurement)
             if turbo.can_stream():
                 return turbo.stream(
-                    [
-                        turbo.append(
-                            template(
-                                "measurements/_measurement.html.j2",
-                                measurement=measurement,
-                            ),
-                            target="measurements",
-                        ),
-                        turbo.update(
-                            template("measurements/_add.html.j2"), target="form"
-                        ),
-                    ]
+                    turbo.replace(
+                        template("measurements/_edit.html.j2", measurement=measurement),
+                        target=f"measurement-{measurement.id}",
+                    )
                 )
-        return self.template()
-
-    # def index(self):
-    #     self.measurements = Measurement.load_all()
-    #     return self.template()
+            else:
+                return self.template(
+                    template_name="/measurements/index.html.j2", edit_id=measurement.id
+                )
 
     @route("/edit/<id>", methods=["GET", "POST"])
     def edit(self, id):
-        self.measurements = Measurement.load_all()
         measurement = Measurement.load(id)
         if request.method == "POST":
             measurement.name = request.form["measurement"]
             measurement.save()
-            return redirect(url_for("MeasurementsView:index"))
+            if turbo.can_stream():
+                return turbo.stream(
+                    turbo.replace(
+                        template(
+                            "measurements/_measurement.html.j2", measurement=measurement
+                        ),
+                        target=f"measurement-{measurement.id}",
+                    )
+                )
+            else:
+                return redirect(url_for("MeasurementsView:index"))
 
-        return self.template(
-            template_name="/measurements/index.html.j2", edit_id=measurement.id
-        )
+        # elif request.method == "GET":
+        #     # WIP - currently cannot stream here, so it's always `else`
+        #     if turbo.can_stream():
+        #         print("can_stream")
+        #         return turbo.stream(
+        #             turbo.replace(
+        #                 template("measurements/_edit.html.j2", measurement=measurement),
+        #                 target=f"measurement-{measurement.id}",
+        #             )
+        #         )
+        else:
+            return self.template(
+                template_name="/measurements/index.html.j2", edit_id=measurement.id
+            )
+
+    @route("/create", methods=["POST"])
+    def create(self):
+        measurement = Measurement(name=request.form["measurement"])
+        measurement.save()
+
+        if turbo.can_stream():
+            return turbo.stream(
+                [
+                    turbo.append(
+                        template(
+                            "measurements/_measurement.html.j2", measurement=measurement
+                        ),
+                        target="measurements",
+                    ),
+                    turbo.update(
+                        template("measurements/_add.html.j2"),
+                        target="measurement-create-form",
+                    ),
+                ]
+            )
+        else:
+            self.measurements.append(measurement)
+            return self.template()

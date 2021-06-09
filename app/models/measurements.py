d7c19b59de412dc301a965d472f28b7f0873287e
Jan Peterka <jan.peterka@hey.com>
2021-06-09 10:59:58 +0200

    Add columns

--- app/models/measurements.py
+++ app/models/measurements.py
@@ -11,7 +11,14 @@ class Measurement(db.Model, BaseMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(255))
-    # suffix = db.Column(db.String(20))
+    suffix = db.Column(db.String(20))
+
+    parent_measurement_id = db.Column(db.ForeignKey("measurements.id"))
+    parent_measurement_factor = db.Column(db.Float())
+
+    parent_measurement = db.relationship(
+        "User", uselist=False, backref="child_measurements"
+    )
 
     # LOADERS
     @staticmethod

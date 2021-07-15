2475aecd9d8eeb66401536efb5295df8b20eb584
Jan Peterka <jan.peterka@hey.com>
2021-07-15 12:02:51 +0200

    Add admin dashboard

--- app/controllers/admin_statistics.py
+++ app/controllers/admin_statistics.py
@@ -1,4 +1,4 @@
-from flask_security import login_required
+from flask_security import login_required, roles_accepted
 
 from app.helpers.helper_flask_view import HelperFlaskView
 
@@ -6,7 +6,10 @@ from app.models.tips import Tip
 
 
 class AdminStatisticsView(HelperFlaskView):
-    decorators = [login_required]
+    decorators = [login_required, roles_accepted("admin", "application_manager")]
+
+    def index(self):
+        return self.template(template_name="admin/index.html.j2")
 
     def unapproved_tips_count(self):
         self.unapproved_tips = Tip.unapproved_tips()

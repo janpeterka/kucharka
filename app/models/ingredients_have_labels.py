8b1e1f538c58e84c8afa0082341027aa82340c9a
Jan Peterka <jan.peterka@hey.com>
2021-09-26 23:10:34 +0200

    Add basic models

---
+++ app/models/ingredients_have_labels.py
@@ -0,0 +1,26 @@
+from app import db, BaseModel
+
+# from flask_security import current_user
+
+from app.helpers.base_mixin import BaseMixin
+
+
+class IngredientHasLabel(BaseModel, BaseMixin):
+    __tablename__ = "ingredients_have_labels"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    ingredient_id = db.Column(
+        db.ForeignKey("ingredients.id"), nullable=False, index=True
+    )
+    label_id = db.Column(db.ForeignKey("labels.id"), nullable=False, index=True)
+
+    # ingredient = db.relationship("Ingredient", backref="ingredient_labels")
+    # label = db.relationship("Label", backref="ingredient_labels")
+
+    # # LOADERS
+
+    # @staticmethod
+    # def load_by_ingredient(ingredient):
+    #     return IngredientHasLabel.load_all_by_attribute("ingredient_id", ingredient.id)
---
+++ app/models/labels.py
@@ -0,0 +1,14 @@
+from app import db, BaseModel
+
+from app.helpers.base_mixin import BaseMixin
+
+
+class Label(BaseModel, BaseMixin):
+    __tablename__ = "label"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    category_id = db.Column(db.ForeignKey("label_categories.id"))
+    category = db.relationship("LabelCategory", uselist=False, backref="labels")

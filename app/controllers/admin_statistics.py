6479c233a116c1f08cdd622d6f424107fdd81178
Jan Peterka <jan.peterka@hey.com>
2021-09-27 00:28:07 +0200

    Add conversion to labels

--- app/controllers/admin_statistics.py
+++ app/controllers/admin_statistics.py
@@ -1,3 +1,5 @@
+from flask import redirect
+from flask_classful import route
 from flask_security import login_required, roles_accepted
 
 from app.helpers.helper_flask_view import HelperFlaskView
@@ -20,3 +22,25 @@ class AdminStatisticsView(HelperFlaskView):
         self.unapproved_tips = Tip.unapproved_tips()
         self.notification_count = len(self.unapproved_tips)
         return self.template(template_name="admin/_admin_notification_count.html.j2")
+
+    @route("convert_ingredients_to_labels", methods=["POST"])
+    def convert_ingredients_to_labels(self):
+        from app.models.labels import Label
+        from app.models.ingredients import Ingredient
+
+        for ingredient in Ingredient.load_all():
+            if ingredient.is_vegetarian:
+                ingredient.labels.append(Label.load_by_name("vegetarian"))
+
+            if ingredient.is_vegan:
+                ingredient.labels.append(Label.load_by_name("vegan"))
+
+            if ingredient.lactose_free:
+                ingredient.labels.append(Label.load_by_name("lactose_free"))
+
+            if ingredient.gluten_free:
+                ingredient.labels.append(Label.load_by_name("gluten_free"))
+
+            ingredient.edit()
+
+        return redirect("AdminStatisticsView:index")

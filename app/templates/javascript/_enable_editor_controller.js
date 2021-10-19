56002f58ccba9db8cbd84cf85678a0be7612f06e
Jan Peterka <jan.peterka@hey.com>
2021-10-19 11:01:31 +0200

    Replace summernote with trumbowyg

---
+++ app/templates/javascript/_enable_editor_controller.js
@@ -0,0 +1,14 @@
+Stimulus.register("enable-editor", class extends Controller {
+    static get targets() {
+      return ["editor"]
+    }
+
+  connect() {
+    if (this.hasEditorTarget) {
+      $(this.editorTarget).trumbowyg();
+
+
+    }
+  }
+
+})

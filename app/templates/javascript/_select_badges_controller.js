16dafef8def6bb230f1d65e07ddb6b6761bf7b49
Jan Peterka <jan.peterka@hey.com>
2021-10-25 19:43:40 +0200

    Add badge controller

---
+++ app/templates/javascript/_select_badges_controller.js
@@ -0,0 +1,71 @@
+Stimulus.register("select-badges", class extends Controller {
+    static targets = ["select", "badges"]
+    static classes = ["badge"]
+
+  connect() {
+    this.add_badges();
+    this.hide_select();
+  }
+
+  hide_select(){
+    this.selectTarget.style.display = "none";
+  }
+
+  add_badges(){
+    for (let i = 0, option; option = this.selectTarget.options[i]; i++) {
+      if (option.value > 0) {
+        this.add_badge(option)
+      }
+    }
+  }
+
+  add_badge(option){
+    var badge = document.createElement("span")
+    badge.className = "select-badge btn ms-1 me-1 bg-color-grey"
+    badge.dataset.label = option.label
+    // badge.dataset.value = option.value
+    badge.dataset.selectBadgesValueParam = option.value
+    badge.id = `badge-${option.value}`
+    badge.dataset.action = "click->select-badges#select"
+    badge.innerHTML = option.label
+
+    if (option.selected === true) {
+      this.set_selected_badge(badge)
+    }
+
+    this.badgesTarget.appendChild(badge)
+  }
+
+  select(event){
+    this.set_select_value(event.params.value)
+  }
+
+  set_select_value(value){
+    if (this.selectTarget.value == value) {
+      this.unset_all_badges()
+      this.selectTarget.value = "__None";
+    } else {
+    this.selectTarget.value = value;
+
+    this.unset_all_badges()
+
+    var badge = document.getElementById(`badge-${value}`)
+    this.set_selected_badge(badge)
+    }
+  }
+
+  set_selected_badge(badge){
+    badge.dataset.selected = true
+    badge.classList.remove("bg-color-grey")
+    badge.classList.add("bg-color-light-green")
+  }
+
+  unset_all_badges(){
+    var badges = document.getElementsByClassName("select-badge")
+    for (let i = 0, badge; badge = badges[i]; i++) {
+      badge.classList.remove("bg-color-light-green")
+      badge.classList.add("bg-color-grey")
+      badge.dataset.selected = false
+    }
+  }
+})

5b9d4eb47cf08f7b1e1c71e5cb25eb465c514804
Jan Peterka <jan.peterka@hey.com>
2021-11-03 07:55:04 +0100

    Add after and before

--- app/helpers/turbo.py
+++ app/helpers/turbo.py
@@ -1,5 +1,12 @@
-def remove(self, target):
-    """Create a remove stream.
+def after(self, target):
+    """Create a after stream.
     :param target: the target ID for this change.
     """
-    return self._make_stream("remove", "", target)
+    return self._make_stream("after", "", target)
+
+
+def before(self, target):
+    """Create a before stream.
+    :param target: the target ID for this change.
+    """
+    return self._make_stream("before", "", target)

0334358ea557f9c6e74595a1dfe765a2b4593c67
Jan Peterka <jan.peterka@hey.com>
2021-11-12 11:38:42 +0100

    Improve code

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,10 +1,10 @@
 from app.models.users import User
 
 
-def with_authenticated_user(app, username=None):
+def with_authenticated_user(app, username):
     @app.login_manager.request_loader
     def load_user_from_request(request, username=username):
-        user = User.query.first() if not username else User.load_by_username(username)
+        user = User.load_by_username(username)
         if user is None:
             assert False
         else:

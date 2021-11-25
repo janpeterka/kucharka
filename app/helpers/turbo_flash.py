8809f49f8573f9e54a8d73d59d2ad10d9b958185
Jan Peterka <jan.peterka@hey.com>
2021-11-25 13:43:51 +0100

    rename function

--- app/helpers/turbo_flash.py
+++ app/helpers/turbo_flash.py
@@ -6,15 +6,14 @@ from app import turbo
 
 def turbo_flash(message, category=None):
     if turbo.can_stream():
-        _turbo_flash(message, category)
+        save_flash_to_session(message, category)
     else:
         flash(message, category)
 
 
-def _turbo_flash(message, category):
+def save_flash_to_session(message, category):
     session["turbo_flash_message"] = message
     session["turbo_flash_message_category"] = category
-    print(category)
 
 
 def is_flash_in_session() -> bool:

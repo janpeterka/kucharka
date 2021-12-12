e86b000d9465c7070d250ee304a4a125c8868a08
Jan Peterka <jan.peterka@hey.com>
2021-12-12 17:48:17 +0100

    Add more tests

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,8 +1,3 @@
-# from flask_dance.consumer.storage import MemoryStorage
-
-# from app import google_oauth_bp
-
-
 # GOOGLE OAUTH
 # google login existing user with oauth
 # should login
@@ -12,30 +7,6 @@
 
 # google login noexisting user without oauth
 # should create user (with invalid password hash), oauth, and login user
-
-
-# def test_index_unauthorized(monkeypatch):
-#     storage = MemoryStorage()
-#     monkeypatch.setattr(google_oauth_bp, "storage", storage)
-
-#     with app.test_client() as client:
-#         response = client.get("/", base_url="https://example.com")
-
-#     assert response.status_code == 302
-#     assert response.headers["Location"] == "https://example.com/login/github"
-
-
-# def test_index_authorized(monkeypatch):
-#     storage = MemoryStorage({"access_token": "fake-token"})
-#     monkeypatch.setattr(google_oauth_bp, "storage", storage)
-
-#     with app.test_client() as client:
-#         response = client.get("/")
-
-#     assert response.status_code == 200
-#     # text = response.get_data(as_text=True)
-#     # assert text == "You are authorized"
-
 
 # AUTH
 
@@ -54,11 +25,13 @@ def test_register_existing(app, db, client):
 # register - invalid email
 def test_register_invalid(app, db, client):
     response = _register(client, "admin", "somepassword")
-    print(response.data)
     assert b"Invalid email address" in response.data
 
 
 # login user
+def test_login(app, db, client):
+    response = _login(client, "admin@sk.cz", "pass123")
+    assert b"V\xc3\xadtejte!" in response.data
 
 
 def _register(client, email, password):

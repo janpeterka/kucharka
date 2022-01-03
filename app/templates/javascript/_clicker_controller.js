c49d8357246787c1773cb888374dd7f887220755
Jan Peterka <jan.peterka@hey.com>
2022-01-03 16:27:43 +0100

    Improve design, clickable links

--- app/templates/javascript/_clicker_controller.js
+++ app/templates/javascript/_clicker_controller.js
@@ -12,9 +12,10 @@ Stimulus.register("clicker", class extends Controller {
   }
 
   activateLink(event) {
+    // console.log(document.elementFromPoint(event.clientX, event.clientY))
     var first_child_link = event.target.getElementsByTagName("a")[0];
+    // console.log(first_child_link)
     first_child_link.click();
-    // console.log(event.target.getElementsByTagName("a")[0])
   }
 
 })

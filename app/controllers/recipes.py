01946e553bc178345eb570df59e2ef1bedd81ebf
Jan Peterka <jan.peterka@hey.com>
2022-01-27 15:56:39 +0100

    Random code refactors (#227)
    * Move import
    
    * Move get_request_portion_count
    
    * Move before_ code to respective methods

--- app/controllers/recipes.py
+++ app/controllers/recipes.py
@@ -8,6 +8,8 @@ from flask_security import login_required, permissions_required, current_user
 
 from flask_classful import route
 
+from flask_weasyprint import render_pdf, HTML
+
 from app.modules.files import PhotoForm
 
 from app.helpers.turbo_flash import turbo_flash as flash
@@ -19,55 +21,41 @@ from app.models.recipes import Recipe
 from app.controllers.forms.recipes import RecipesForm
 
 
+def get_portion_count(request):
+    request_portion_count = request.args.get("portion_count", "1")
+
+    if not request_portion_count:
+        request_portion_count = 1
+
+    return int(request_portion_count)
+
+
 class RecipesView(HelperFlaskView):
     def before_request(self, name, id=None, **kwargs):
         self.recipe = Recipe.load(id)
+
         if current_user.is_authenticated:
             self.validate_operation(id, self.recipe)
 
-        if name in ["index"] and current_user.is_authenticated:
-            self.recipes = sorted(
-                current_user.visible_recipes,
-                key=lambda x: unidecode(x.name.lower()),
-            )
-
-        if name in ["show", "pdf", "show_pdf"] and "portion_count" in request.args:
-            request_portion_count = request.args.get("portion_count", "1")
-            if not request_portion_count:
-                request_portion_count = 1
-
-            self.recipe.portion_count = int(request_portion_count)
-
-    @login_required
-    def before_edit(self, id):
-        self.form = create_form(RecipesForm, obj=self.recipe)
-
-        self.personal_ingredients = sorted(
-            self.recipe.unused_personal_ingredients,
-            key=lambda x: unidecode(x.name.lower()),
-        )
-
-        self.public_ingredients = sorted(
-            self.recipe.unused_public_ingredients,
-            key=lambda x: unidecode(x.name.lower()),
-        )
-
-    @login_required
-    def before_new(self):
-        self.form = create_form(RecipesForm)
-
     @login_required
     def index(self):
+        self.recipes = sorted(
+            current_user.visible_recipes,
+            key=lambda x: unidecode(x.name.lower()),
+        )
+
         return self.template()
 
     @login_required
     @permissions_required("manage-application")
     def all(self):
         self.recipes = Recipe.load_all()
+
         return self.template()
 
     # @login_required
     def show(self, id):
+        self.recipe.portion_count = get_portion_count(request)
         self.photo_form = PhotoForm()
 
         if not current_user.is_authenticated:
@@ -77,10 +65,11 @@ class RecipesView(HelperFlaskView):
         return self.template()
 
     def show_pdf(self, id):
+        self.recipe.portion_count = get_portion_count(request)
         return self.template(template_name="show", print=True)
 
     def pdf(self, id):
-        from flask_weasyprint import render_pdf, HTML
+        self.recipe.portion_count = get_portion_count(request)
 
         return render_pdf(HTML(string=self.template(template_name="show", print=True)))
 
@@ -102,10 +91,24 @@ class RecipesView(HelperFlaskView):
 
     @login_required
     def edit(self, id):
+        self.form = create_form(RecipesForm, obj=self.recipe)
+
+        self.personal_ingredients = sorted(
+            self.recipe.unused_personal_ingredients,
+            key=lambda x: unidecode(x.name.lower()),
+        )
+
+        self.public_ingredients = sorted(
+            self.recipe.unused_public_ingredients,
+            key=lambda x: unidecode(x.name.lower()),
+        )
+
         return self.template()
 
     @login_required
     def new(self):
+        self.form = create_form(RecipesForm)
+
         return self.template()
 
     @login_required

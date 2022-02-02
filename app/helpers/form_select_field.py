0f559e9c9c71eeddd6360db9d572880165f07821
Jan Peterka <jan.peterka@hey.com>
2022-02-02 11:09:02 +0100

    Add data-attributes to select > options (#207)
    * Add custom QuerySelectField
    
    * Add CustomSelect widget, POC
    
    * Working version with colors, badges,
    
    * 'Refactored by Sourcery' (#224)
    
    Co-authored-by: Sourcery AI <>
    
    * Remove unused fields
    
    * Rename Widget, fix badge controller
    
    * Again hide select
    
    * Set default color if none in database
    
    * Simplify and comment ExtendedSelectWidget
    
    * Add space, add unused visibility-fadein
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ app/helpers/form_select_field.py
@@ -0,0 +1,38 @@
+from wtforms.widgets import html_params
+from flask import Markup
+
+
+class ExtendedSelectWidget:
+    """
+    Renders a select field allowing custom attributes for options.
+    Expects the field to be an iterable object of Option fields.
+    The render function accepts a dictionary of option ids ("{field_id}-{option_index}")
+    which contain a dictionary of attributes to be passed to the option.
+
+    Example:
+    form.customselect(option_attr={"customselect-0": {"disabled": ""} })
+    """
+
+    def __init__(self, multiple=False):
+        self.multiple = multiple
+
+    def __call__(self, field, option_attr={}, **kwargs):
+
+        kwargs.setdefault("id", field.id)
+
+        if self.multiple:
+            kwargs["multiple"] = True
+
+        if "required" not in kwargs and "required" in getattr(field, "flags", []):
+            kwargs["required"] = True
+
+        html = ["<select %s>" % html_params(name=field.name, **kwargs)]
+
+        for option in field:
+            # option.id - id of field. Example: 'custom_select-0'
+            attributes = option_attr.get(option.id, {})
+            html.append(option(**attributes))
+
+        html.append("</select>")
+
+        return Markup("".join(html))

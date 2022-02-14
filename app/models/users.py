3383d72fbf6201b87d823ff334253efbc57829c0
Jan Peterka <jan.peterka@hey.com>
2022-02-14 09:20:07 +0100

    Show also shared events on dashboard

--- app/models/users.py
+++ app/models/users.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 from flask_security.models.fsqla_v2 import FsUserMixin as UserMixin
 from flask_security import hash_password
 
@@ -25,6 +27,7 @@ class User(BaseModel, BaseMixin, UserMixin, CalendarUserMixin):
     recipes = db.relationship("Recipe", order_by="Recipe.name", back_populates="author")  # type: ignore
 
     events = db.relationship("Event", back_populates="author")  # type: ignore
+
     role_events = db.relationship(
         "Event",
         secondary="users_have_event_roles",
@@ -65,22 +68,40 @@ class User(BaseModel, BaseMixin, UserMixin, CalendarUserMixin):
         return self.password != "x"
 
     @property
+    def all_events(self):
+        return self.events + self.role_events
+
+    @property
     def active_events(self):
         return [e for e in self.events if e.is_active]
+
+    @property
+    def all_active_events(self):
+        return [e for e in self.all_events if e.is_active]
 
     @property
     def active_future_events(self):
         return [e for e in self.active_events if e.in_future]
 
     @property
+    def all_active_future_events(self):
+        return [e for e in self.all_active_events if e.in_future]
+
+    @property
     def closest_future_event(self):
         if not self.active_future_events:
             return None
 
-        closest_event = self.active_future_events[0]
-        for event in self.active_future_events:
-            if event.date_from > closest_event.date_from:
-                closest_event = event
+        closest_event = min(self.active_future_events, key=attrgetter("date_from"))
+
+        return closest_event
+
+    @property
+    def all_closest_future_event(self):
+        if not self.all_active_future_events:
+            return None
+
+        closest_event = min(self.all_active_future_events, key=attrgetter("date_from"))
 
         return closest_event
 

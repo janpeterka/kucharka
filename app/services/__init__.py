9c52d00fc61325283e9721c5429b422283b635e5
Jan Peterka <jan.peterka@hey.com>
2022-07-09 13:29:48 +0200

    Add empty service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,11 @@
 from .events.role_manager import EventRoleManager
 from .events.timetable_contructor import EventTimetableConstructor
+from .events.shopping_contructor import EventShoppingConstructor
 from .tips.approver import TipApprover
 
-__all__ = ["EventRoleManager", "EventTimetableConstructor", "TipApprover"]
+__all__ = [
+    "EventRoleManager",
+    "EventTimetableConstructor",
+    "EventShoppingConstructor",
+    "TipApprover",
+]

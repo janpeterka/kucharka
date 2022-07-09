1964c8387e8fba2cbbdb64da080c7e0070caef9d
Jan Peterka <jan.peterka@hey.com>
2022-07-09 13:49:10 +0200

    Move code to service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,11 +1,11 @@
 from .events.role_manager import EventRoleManager
-from .events.timetable_contructor import EventTimetableConstructor
-from .events.shopping_contructor import EventShoppingConstructor
+from .events.timetable_constructor import EventTimetableConstructor
+from .events.shopping_list_constructor import ShoppingListConstructor
 from .tips.approver import TipApprover
 
 __all__ = [
     "EventRoleManager",
     "EventTimetableConstructor",
-    "EventShoppingConstructor",
+    "ShoppingListConstructor",
     "TipApprover",
 ]

35ea6e7a42ebedb6493e1839c197b3832bae8e88
Jan Peterka <jan.peterka@hey.com>
2022-07-09 14:12:48 +0200

    Improve controller

--- app/controllers/event_exporter.py
+++ app/controllers/event_exporter.py
@@ -18,19 +18,39 @@ class EventExporterView(HelperFlaskView):
         self.event = Event.load(event_id)
         self.validate_show(self.event)
 
-    def show_list(self, event_id, show_type):
-        return self._show_list(event_id, show_type)
+    def show_list(self, event_id):
+        return self._show_list(event_id)
 
-    def show_list_pdf(self, event_id, show_type):
-        string = self._show_list(event_id, show_type, is_print=True)
+    def show_table(self, event_id):
+        return self._show_table(event_id)
+
+    def show_list_pdf(self, event_id):
+        string = self._show_list(event_id, is_print=True)
+
         return render_pdf(
             HTML(string=string),
             download_filename=f"{self.event.slugified_name}--nakupni-seznam.pdf",
             automatic_download=False,
         )
 
-    def download_list_pdf(self, event_id, show_type):
-        string = self._show_list(event_id, show_type, is_print=True)
+    def show_table_pdf(self, event_id):
+        string = self._show_table(event_id, is_print=True)
+
+        return render_pdf(
+            HTML(string=string),
+            download_filename=f"{self.event.slugified_name}--nakupni-seznam.pdf",
+            automatic_download=False,
+        )
+
+    def download_list_pdf(self, event_id):
+        string = self._show_list(event_id, is_print=True)
+        return render_pdf(
+            HTML(string=string),
+            download_filename=f"{self.event.slugified_name}--nakupni-seznam.pdf",
+        )
+
+    def download_table_pdf(self, event_id):
+        string = self._show_list(event_id, is_print=True)
         return render_pdf(
             HTML(string=string),
             download_filename=f"{self.event.slugified_name}--nakupni-seznam.pdf",
@@ -39,46 +59,32 @@ class EventExporterView(HelperFlaskView):
     # Shopping list export
 
     def show_shopping_list(self, event_id):
-        return redirect(
-            url_for("EventExporterView:show_list", event_id=event_id, show_type="table")
-        )
+        return redirect(url_for("EventExporterView:show_table", event_id=event_id))
 
     def show_shopping_list_pdf(self, event_id):
-        return redirect(
-            url_for(
-                "EventExporterView:show_list_pdf", event_id=event_id, show_type="table"
-            )
-        )
+        return redirect(url_for("EventExporterView:show_table_pdf", event_id=event_id))
 
     def download_shopping_list_pdf(self, event_id):
         return redirect(
             url_for(
-                "EventExporterView:download_list_pdf",
+                "EventExporterView:download_table_pdf",
                 event_id=event_id,
-                show_type="table",
             )
         )
 
     # Ingredient list export
 
     def show_ingredient_list(self, event_id):
-        return redirect(
-            url_for("EventExporterView:show_list", event_id=event_id, show_type="list")
-        )
+        return redirect(url_for("EventExporterView:show_list", event_id=event_id))
 
     def show_ingredient_list_pdf(self, event_id):
-        return redirect(
-            url_for(
-                "EventExporterView:show_list_pdf", event_id=event_id, show_type="list"
-            )
-        )
+        return redirect(url_for("EventExporterView:show_list_pdf", event_id=event_id))
 
     def download_ingredient_list_pdf(self, event_id):
         return redirect(
             url_for(
                 "EventExporterView:download_list_pdf",
                 event_id=event_id,
-                show_type="list",
             )
         )
 
@@ -142,25 +148,31 @@ class EventExporterView(HelperFlaskView):
 
     # INTERNAL
 
-    def _show_list(self, event_id, show_type, is_print=False):
-        shopping_list_constructor = ShoppingListConstructor(event=self.event)
+    def _set_values(self):
+        self.shopping_list_constructor = ShoppingListConstructor(event=self.event)
 
         self.lasting_ingredients_shopping = (
-            shopping_list_constructor.get_lasting_shopping()
+            self.shopping_list_constructor.get_lasting_shopping()
         )
-        self.shoppings = shopping_list_constructor.get_shoppings()
+        self.shoppings = self.shopping_list_constructor.get_shoppings()
 
         self.lasting_ingredients_shopping.recipe_ingredient_amounts = (
-            shopping_list_constructor.get_amounts_for_shopping(
+            self.shopping_list_constructor.get_amounts_for_shopping(
                 self.lasting_ingredients_shopping
             )
         )
 
         for shopping in self.shoppings:
             shopping.recipe_ingredient_amounts = (
-                shopping_list_constructor.get_amounts_for_shopping(shopping)
+                self.shopping_list_constructor.get_amounts_for_shopping(shopping)
             )
 
-        return self.template(
-            template_name="shopping_list", show_type=show_type, print=is_print
-        )
+    def _show_list(self, event_id, is_print=False):
+        self._set_values()
+
+        return self.template(template_name="shopping_list", print=is_print)
+
+    def _show_table(self, event_id, is_print=False):
+        self._set_values()
+
+        return self.template(template_name="shopping_table", print=is_print)

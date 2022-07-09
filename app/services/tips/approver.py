0cba73d77d4937470057ff41b2835b78a7c2097a
Jan Peterka <jan.peterka@hey.com>
2022-07-09 13:05:46 +0200

    Change to static methods

--- app/services/tips/approver.py
+++ app/services/tips/approver.py
@@ -1,9 +1,11 @@
 class TipApprover:
+    @staticmethod
     def approve(tip):
         tip.is_approved = True
         tip.is_hidden = False
         tip.save()
 
+    @staticmethod
     def disapprove(tip):
         tip.is_approved = False
         tip.is_hidden = True

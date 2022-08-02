905e2e0a7082811176b0a9a0a60a1b1a270ad3d4
Jan Peterka <jan.peterka@hey.com>
2022-08-02 18:33:22 +0200

    Move DailyPlanRecipes logic to service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,10 +2,12 @@ from .events.role_manager import EventRoleManager
 from .events.timetable_constructor import EventTimetableConstructor
 from .events.shopping_list_constructor import ShoppingListConstructor
 from .tips.approver import TipApprover
+from .daily_plans.manager import DailyPlanManager
 
 __all__ = [
     "EventRoleManager",
     "EventTimetableConstructor",
     "ShoppingListConstructor",
     "TipApprover",
+    "DailyPlanManager",
 ]

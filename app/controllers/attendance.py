3d0ad2a6e49556f4b3d6e7fb99f41b387a8485ac
Jan Peterka <jan.peterka@hey.com>
2022-08-04 11:37:02 +0200

    Move code

--- app/controllers/attendance.py
+++ app/controllers/attendance.py
@@ -1,6 +1,5 @@
-from flask import request, redirect, url_for
+from flask import request
 from flask_security import login_required
-from flask_classful import route
 
 from app.helpers.helper_flask_view import HelperFlaskView
 from app.helpers.form import create_form
@@ -12,34 +11,12 @@ from app.forms import AttendeeForm
 class AttendanceView(HelperFlaskView):
     decorators = [login_required]
 
+    def before_request(self, name, event_id, **kwargs):
+        self.event = Event.load(event_id)
+        self.validate_show(self.event)
+
     def before_index(self, event_id):
-        self.event = Event.load(event_id)
-        self.validate_show(self.event)
         self.edit_id = int(request.args.get("edit_id", 0))
-
-    def before_update(self, attendee_id):
-        self.attendee = Attendee.load(attendee_id)
-        self.validate_edit(self.attendee.event)
-
-    def before_export(self, event_id):
-        self.event = Event.load(event_id)
-        self.validate_show(self.event)
-
-    def before_post(self, event_id, portion_type_id):
-        self.event = Event.load(event_id)
-        self.validate_edit(self.event)
-
-    def before_delete(self, attendee_id):
-        self.attendee = Attendee.load(attendee_id)
-        self.validate_edit(self.attendee.event)
-
-    def post(self, event_id, portion_type_id):
-        form = AttendeeForm(request.form)
-        attendee = Attendee(event_id=event_id, portion_type_id=portion_type_id)
-        attendee.fill(form)
-        attendee.save()
-
-        return redirect(url_for("AttendanceView:index", event_id=event_id))
 
     def index(self, event_id):
         self.form = create_form(AttendeeForm)
@@ -51,20 +28,3 @@ class AttendanceView(HelperFlaskView):
 
     def export(self, event_id):
         return self.template()
-
-    @route("update/<attendee_id>", methods=["POST"])
-    def update(self, attendee_id):
-        form = AttendeeForm(request.form)
-
-        self.attendee.fill(form)
-        self.attendee.edit()
-
-        return redirect(
-            url_for("AttendanceView:index", event_id=self.attendee.event.id)
-        )
-
-    @route("delete/<attendee_id>", methods=["POST"])
-    def delete(self, attendee_id):
-        self.attendee.delete()
-
-        return redirect(request.referrer)

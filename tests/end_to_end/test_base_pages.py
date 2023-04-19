113113635e28860d30329fba5ec0d4c60b5615bb
Jan Peterka <jan.peterka@hey.com>
2023-04-19 15:01:01 +0200

    Update tests

--- tests/end_to_end/test_base_pages.py
+++ tests/end_to_end/test_base_pages.py
@@ -4,21 +4,26 @@ from tests.helpers import playwright_login
 
 
 @pytest.mark.integration
-def test_unlogged(live_server, page: Page):
-    page.goto("dashboard")
-    expect(page.locator("body")).to_contain_text("přihlášení")
-
-
-@pytest.mark.integration
 def test_base_pages(db, live_server, page: Page):
     playwright_login(page)
-
-    import time
 
     page.goto("/")
     expect(page.locator("body")).to_contain_text("vítej!")
 
-    page.goto("/event/")
-    # expect(page.locator("body")).to_contain_text("událost")
-    # expect(page.locator("body")).to_contain_text("opakovaná událost")
-    # expect(page.locator("body")).to_contain_text("minulé události")
+    # page.goto("/ingredient/")
+    page.locator('a:has-text("suroviny")').click()
+    expect(page.locator("body")).to_contain_text("moje suroviny")
+
+    # page.goto("/recipe/")
+    page.locator('a:has-text("recepty")').click()
+    expect(page.locator("body")).to_contain_text("moje recepty")
+
+    # page.goto("/event/")
+    page.locator('a:has-text("akce")').click()
+    expect(page.locator("body")).to_contain_text("moje akce")
+
+    page.locator('a:has-text("databáze receptů")').click()
+    expect(page.locator("body")).to_contain_text("pokročilé filtrování")
+
+    page.locator('a:has-text("tipy a triky")').click()
+    expect(page.locator("body")).to_contain_text("tipy a návody")

b15cad281856008cd903a106cb94107516236247
Jan Peterka <jan.peterka@hey.com>
2023-05-18 16:07:39 +0200

    Try signin with passwordless in playground (#464)
    * Add passwordless login to playground
    
    * Add feedback

--- app/modules/auth/passwordless.py
+++ app/modules/auth/passwordless.py
@@ -8,8 +8,6 @@ passwordless = Blueprint("passwordless", __name__)
 
 @passwordless.route("register-token")
 def register_token():
-    print("trying to add token...")
-
     payload = {
         "userId": current_user.id,
         "username": current_user.email,
@@ -19,36 +17,44 @@ def register_token():
         "ApiSecret": app.config["PASSWORDLESS_SECRET"],
         "Content-Type": "application/json",
     }
+
     response = requests.post(
         f"{app.config['PASSWORDLESS_URL']}/register/token",
         json=payload,
         headers=headers,
     )
-    print(response.__dict__)
-    print(response.json())
+
     token = response.json().get("token")
     # Process the token or perform any additional logic here
     return {"token": token}
 
 
-# @passwordless.route("/signin/verify", methods=["POST"])
-# def verify_sign_in():
-#     try:
-#         token = request.json.get("token")
+@passwordless.route("/signin", methods=["POST"])
+def verify_sign_in():
+    try:
+        token = request.json.get("token")
 
-#         api_secret = app.config["PASSWORDLESS_SECRET"]
-#         headers = {"ApiSecret": api_secret, "Content-Type": "application/json"}
-#         response = requests.post(
-#             "https://v4.passwordless.dev/signin/verify", json=token, headers=headers
-#         )
-#         body = response.json()
+        api_secret = app.config["PASSWORDLESS_SECRET"]
+        headers = {"ApiSecret": api_secret, "Content-Type": "application/json"}
+        response = requests.post(
+            "https://v4.passwordless.dev/signin/verify", json=token, headers=headers
+        )
+        body = response.json()
 
-#         if body.get("success"):
-#             print("Successfully verified sign-in for user:", body)
-#             # Set a cookie/userid or perform additional logic here
-#         else:
-#             print("Sign-in failed:", body)
+        if body.get("success"):
+            print("Successfully verified sign-in for user:", body)
+            # Set a cookie/userid or perform additional logic here
+        else:
+            print("Sign-in failed:", body)
 
-#         return jsonify(body)
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
+        from app.models import User
+
+        # from flask_security import login_user
+        from flask import flash
+
+        # login_user(User.load(body["userId"]))
+        flash(f"Successfully logged in {User.load(body['userId'])}", "success")
+
+        return jsonify(body)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500

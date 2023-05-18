63a1b28be3e67eb6ed28fdcd6c2fcaea8588ffe4
Jan Peterka <jan.peterka@hey.com>
2023-05-18 18:10:43 +0200

    send some sentry messages

--- app/modules/auth/passwordless.py
+++ app/modules/auth/passwordless.py
@@ -22,6 +22,7 @@ def register_token():
         f"{app.config['PASSWORDLESS_URL']}/register/token",
         json=payload,
         headers=headers,
+        timeout=5,
     )
 
     token = response.json().get("token")
@@ -32,24 +33,32 @@ def register_token():
 @passwordless.route("/signin", methods=["POST"])
 def verify_sign_in():
     import json
+    from sentry_sdk import capture_message
 
     try:
         token = request.args.get("token")
 
         api_secret = app.config["PASSWORDLESS_SECRET"]
         headers = {"ApiSecret": api_secret, "Content-Type": "application/json"}
+        json = json.dumps({"token": token})
+        capture_message("This is going fine")
+
         response = requests.post(
             "https://v4.passwordless.dev/signin/verify",
-            json=json.dumps({"token": token}),
+            json=json,
             headers=headers,
+            timeout=5,
         )
+        capture_message(f"Their response was: {response}")
         body = response.json()
+        capture_message(f"This is the body: {body}")
+        capture_message(f"This is the body jsonified: {jsonify(body)}")
 
-        if body.get("success"):
-            print("Successfully verified sign-in for user:", body)
-            # Set a cookie/userid or perform additional logic here
-        else:
-            print("Sign-in failed:", body)
+        # if body.get("success"):
+        #     print("Successfully verified sign-in for user:", body)
+        #     # Set a cookie/userid or perform additional logic here
+        # else:
+        #     print("Sign-in failed:", body)
 
         return jsonify(body)
     except Exception as e:

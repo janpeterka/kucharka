2791c41340c81b7260b73ad5f07b41f5d07a2563
Jan Peterka <jan.peterka@hey.com>
2023-05-22 10:31:02 +0200

    Correct trumbowyg

--- app/static/js/controllers/enable_editor_controller.js
+++ app/static/js/controllers/enable_editor_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "../../node_modules/@hotwired/stimulus"
+import trumbowyg from '../../node_modules/trumbowyg';
 
+$.trumbowyg.svgPath = '/static/node_modules/trumbowyg/dist/ui/icons.svg';
+
+// Usage: connect directly on text-area element
 export default class extends Controller {
-    static targets = ["editor"]
 
   connect() {
-    if (this.hasEditorTarget) {
-      $(this.editorTarget).trumbowyg();
-    }
+    $(this.element).trumbowyg();
   }
 
 }

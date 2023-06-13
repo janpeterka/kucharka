c028b0917a76e41ab1b32b563af069b7572f9960
Jan Peterka <jan.peterka@hey.com>
2023-06-13 21:38:43 +0200

    Don't change amount if ingredient is not measured

--- app/services/recipes/ingredient_manager.py
+++ app/services/recipes/ingredient_manager.py
@@ -4,6 +4,7 @@ class RecipeIngredientManager:
 
     def update_ingredient_amounts(self, old_portion_count, new_portion_count):
         for recipe_ingredient in self.recipe.recipe_ingredients:
-            old_total_amount = recipe_ingredient.amount * old_portion_count
-            recipe_ingredient.amount = old_total_amount / new_portion_count
-            recipe_ingredient.edit()
+            if recipe_ingredient.is_measured:
+                old_total_amount = recipe_ingredient.amount * old_portion_count
+                recipe_ingredient.amount = old_total_amount / new_portion_count
+                recipe_ingredient.edit()

6a89a0cd980ffd95c274215fbf8e062adb1f01f6
Jan Peterka <jan.peterka@hey.com>
2023-06-28 19:44:19 +0200

    Prepare loggable ingredient

--- app/models/ingredients.py
+++ app/models/ingredients.py
@@ -14,8 +14,9 @@ class Ingredient(BaseModel, BaseMixin, ItemPresenter):
     name = db.Column(db.String(255), nullable=False)
 
     created_by = db.Column(db.ForeignKey("users.id"), nullable=False, index=True)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    last_updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+    created_at = db.Column(
+        db.DateTime, default=db.func.current_timestamp(), nullable=False
+    )
 
     description = db.Column(db.Text)
     measurement_id = db.Column(db.ForeignKey("measurements.id"))

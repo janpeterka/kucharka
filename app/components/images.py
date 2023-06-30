827177616f160b0cee2821052db73cb11031ca9b
Jan Peterka <jan.peterka@hey.com>
2023-06-30 18:12:16 +0200

    Clean code

--- app/components/images.py
+++ app/components/images.py
@@ -4,25 +4,16 @@ from app.components import BaseComponent
 class Image(BaseComponent):
     def __init__(self, image, **kwargs):
         super(Image, self).__init__(**kwargs)
-        self.src = image.url
+        if kwargs.get("thumbnail", False):
+            self.src = image.thumbnail_url
+        else:
+            self.src = image.url
+
         self.alt = image.full_name
         self.image = image
 
 
 def recipe_gallery_image(image, editable=False, outer_class=None, **kwargs):
-    # {% set class = kwargs.pop('class', "") %}
-    # {% set style = kwargs.pop('style', "") %}
-    # {% set thumbnail = kwargs.pop('thumbnail', False) %}
-    # {% set with_delete = kwargs.pop('with_delete', False) %}
-    # {% set with_pin = kwargs.pop('with_pin', False) %}
-    # {% set center = kwargs.pop('center', False) %}
-
-    # {% if thumbnail %}
-    #     {% set url = image.thumbnail_url %}
-    # {% else %}
-    #     {% set url = image.url %}
-    # {% endif %}
-
     if outer_class is None:
         outer_class = "pos-r"
     else:
